634b31896fa31da3c9f28b262009a67795b8f2d7
daniel nyoho <danielnyoho@gmail.com>
2023-06-23 14:02:11 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
-# create a file at /tmp directory
-# using the following requirements
-
-file { 'school':
-  path    => '/tmp/school',
-  mode    => '0744',
+# Creates a file 
+file { '/tmp/school':
+  ensure  => 'present',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
+  mode    => '0744',
 }
